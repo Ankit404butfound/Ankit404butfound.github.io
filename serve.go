f7384c483294a22dfa10ba76c3dab17c9f2a632f
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-11 10:47:19 +0530

    FIx container

--- serve.go
+++ serve.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
         http.Handle("/", http.FileServer(http.Dir("")))
-        http.ListenAndServe("192.168.0.156:8000", nil)
+        http.ListenAndServe(":8000", nil)
 }
